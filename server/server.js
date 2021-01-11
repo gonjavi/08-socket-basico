46cb8046ff173186bec90f1b93ad90f512d2f334
manci <javiercaliescali@gmail.com>
2021-01-11 14:23:06 -0500

    ordenar codigo

--- server/server.js
+++ server/server.js
@@ -13,39 +13,8 @@ const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 
 // IO = comunicacion con el backend
-let io = socketIO(server);
-
-io.on('connection', (client) => {
-   console.log('Usuario conectado');
-
-   client.on('disconnect', ()=> {
-     console.log('usuario desconectado');
-   });
-
-   // Enviar
-   client.emit('enviarMensaje', {
-     usuario: 'Administrador',
-     mensaje: 'Bienvenido a esta aplicacion'
-   }, function() {
-     console.log('Se disparo el callback');
-   });
-
-   // Escuchar cliente
-   client.on('enviarMensaje', (mensaje, callback)=> {
-    console.log(mensaje);
-
-    if (mensaje.usuario) {
-      callback({
-        resp: 'Todo salió bien!'
-      });
-    } else {
-      callback({
-        resp: 'Todo salió mal!'
-      });
-    }
-    
-   })
-});
+module.exports.io = socketIO(server);
+require('./sockets/socket');
 
 
 server.listen(port, (err) => {
