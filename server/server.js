1b1b9edf45f9a815cc0fa7e7a9da7ff809af0c10
manci <javiercaliescali@gmail.com>
2021-01-11 14:10:08 -0500

    176 retroalimentacion de emisiones del cliente hacia el servidor

--- server/server.js
+++ server/server.js
@@ -22,14 +22,28 @@ io.on('connection', (client) => {
      console.log('usuario desconectado');
    });
 
+   // Enviar
    client.emit('enviarMensaje', {
      usuario: 'Administrador',
      mensaje: 'Bienvenido a esta aplicacion'
+   }, function() {
+     console.log('Se disparo el callback');
    });
 
    // Escuchar cliente
-   client.on('enviarMensaje', (mensaje)=> {
+   client.on('enviarMensaje', (mensaje, callback)=> {
     console.log(mensaje);
+
+    if (mensaje.usuario) {
+      callback({
+        resp: 'Todo salió bien!'
+      });
+    } else {
+      callback({
+        resp: 'Todo salió mal!'
+      });
+    }
+    
    })
 });
 
